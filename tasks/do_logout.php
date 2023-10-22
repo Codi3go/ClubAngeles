304c9f1bb3381cdaa3e348ab25d4a10902cae0a3
Bikcode <bikcodeh@gmail.com>
2023-10-22 16:40:30 -0500

    documentate

--- tasks/do_logout.php
+++ tasks/do_logout.php
@@ -1,6 +1,11 @@
 <?php
+    // invocamos el inicio de sesiones para hacer uso de las mismas
     session_start();
+    // con esta funcion, estamos destruyendo todas las variables de sessiones que pudimos haber creado
     session_destroy();
+    // con esta funcion, estamos liberando todas las variables de sessiones que pudimos haber creado
     session_unset();
+    // y por ultimo como el usuario ya cerró sesion y ya no existen variables de sesion
+    // lo re dirigimos al index para que si quiere, vuelva e inicie sesion
     header("Location: ../index.php");
 ?>
